13378c6071bf27ba136f0bd2dd35e6deb210938e
OyzerovichSeraphim <99070889+OyzerovichSeraphim@users.noreply.github.com>
2023-04-11 13:16:32 +0300

    Add files via upload

--- postgres.sh
+++ postgres.sh
@@ -1,2 +1,10 @@
 sudo apt update
-sudo apt install postgresql
+sudo apt install readline-common
+sudo apt install build-essential automake autoconf
+wget https://ftp.postgresql.org/pub/source/v8.4.0/postgresql-8.4.0.tar.gz
+tar xf postgresql-8.4.0.tar.gz
+cd postgresql-8.4.0
+./configure --without-readline --without-zlib
+make
+sudo su
+make install
